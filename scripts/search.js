5ad0931885ba410cd9307d90be68e8d253c4e17e
emammad22 <mammadoveltun22@gmail.com>
2023-02-07 01:56:18 +0400

    added new functionality

--- scripts/search.js
+++ scripts/search.js
@@ -1,23 +1,49 @@
 var search = document.getElementById('reveal');
 var list = document.getElementById('list');
-
 // event listeners
-search.addEventListener('search', loadProduct);
+search.addEventListener('input', loadProduct);
+const xhr = new XMLHttpRequest();
 
+xhr.open('GET', 'https://www.themealdb.com/api/json/v1/1/filter.php?i=', true);
+
+// for initial view
+xhr.onload = function () {
+    let data = JSON.parse(this.responseText);
+    let html = '';
+
+    data.meals.forEach(element => {
+        html += `
+        <div class="product-card">
+            <div class="prod-img">
+                <img src="${element.strMealThumb}" alt="${element.strMeal}">
+            </div>
+            <div class="product-head">
+                <h2 class="prod-name">${element.strMeal}</h2>
+            </div>
+            <div class="prod-info">
+                <button class="information">Get Information</button>
+            </div>
+        </div>
+        `
+    });
+    list.innerHTML = html;
+
+}
+
+xhr.send();
+
+// searching process
 function loadProduct(e) {
 
     e.preventDefault();
-    const xhr = new XMLHttpRequest();
-
     xhr.open('GET', `https://www.themealdb.com/api/json/v1/1/filter.php?i=${search.value.trim()}`, true);
 
     xhr.onload = function () {
         let products = JSON.parse(this.responseText);
+
         let html = '';
         if (products.meals) {
             products.meals.forEach(element => {
-                console.log(products.meals);
-
                 html += `
                 <div class="product-card">
                     <div class="prod-img">
@@ -36,7 +62,6 @@ function loadProduct(e) {
 
         list.innerHTML = html;
 
-        // }
     }
     xhr.send();
 }
